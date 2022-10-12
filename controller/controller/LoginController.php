96639d678b9184ebf780641f9d1fe35aa66430dd
Yahaya <68204965+yahvya@users.noreply.github.com>
2022-10-12 22:10:29 +0200

    Update LoginController.php

--- controller/controller/LoginController.php
+++ controller/controller/LoginController.php
@@ -9,7 +9,7 @@ class LoginController extends Controller
 	#[Route('get,post','login','log-in')]
 	public function login():void
 	{
-		// can be accessed with 'login/login' or 'login' thanks to the default method override or 'login/log-in' with get method
+		// can be accessed with 'login/login' or 'login' thanks to the default method override or 'login/log-in' with get or post method
 		die('login page');
 	}
 	
